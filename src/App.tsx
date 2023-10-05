66890bd34c5451388a4d58b91232dff3d54422b5
Rodrigoqueiroz25 <digoqueiroz19@gmail.com>
2023-10-05 15:45:53 +0000

    feat: component hero created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Hero from './components/Hero/Hero';
+
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zaink</span>
+      <Hero />
     </div>
   );
 }
